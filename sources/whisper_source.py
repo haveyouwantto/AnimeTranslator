96e14f28cda0f4add6449a143476edcd048995a8
HYWT <haveyouwantto@gmail.com>
2025-04-14 23:16:15 +0800

    whisper source add character

--- sources/whisper_source.py
+++ sources/whisper_source.py
@@ -1,6 +1,9 @@
 
 from models.subtitle import Subtitle, SubtitleSegment
 from .base_source import BaseSubtitleSource
+import logging
+
+logger = logging.getLogger(__name__)
 
 class WhisperSource(BaseSubtitleSource):
     def __init__(self, model_size: str, language: str, beam_size: int = 5):
@@ -12,7 +15,7 @@ class WhisperSource(BaseSubtitleSource):
         from faster_whisper import WhisperModel
         import torch
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        compute_type = 'float32'
+        compute_type = 'float16' if torch.cuda.is_available() else 'float32'
         self.model = WhisperModel(self.model_size, device, compute_type=compute_type)
     
     def get_subtitle(self, audio_path: str) -> Subtitle:
@@ -22,12 +25,16 @@ class WhisperSource(BaseSubtitleSource):
             beam_size=self.beam_size,
             language=self.language
         )
-        subtitle_segments = [
-            SubtitleSegment(
+        subtitle_segments = []
+
+        for i, segment in enumerate(segments):
+            subtitle_segments.append(SubtitleSegment(
                 start=segment.start,
                 end=segment.end,
                 text=segment.text,
-                line_number=i+1  # Whisper生成的行号从1开始
-            ) for i, segment in enumerate(segments)
-        ]
+                line_number=i+1,  # Whisper生成的行号从1开始
+                character="default"
+            ))
+            if i % 10 == 0:
+                logger.info(f'Transcribe at {segment.start}, content: {segment.text}')
         return Subtitle(subtitle_segments)
